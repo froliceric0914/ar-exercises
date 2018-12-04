209cc1219d390ab44ecce446548f9d16450ca47e
froliceric0914 <wzhao.eric@gmail.com>
2018-12-04 21:36:31 +0000

    ex6 done

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,13 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
+class Employee < ActiveRecord::Base
+    belongs_to :Stores
+end
+
+@Store1.employees.create(first_name: "Khurram", last_name: "Peterson", hourly_rate: 60)
+@Store1.employees.create(first_name: "Jason", last_name: "w", hourly_rate: 70)
+@Store2.employees.create(first_name: "Quin", last_name: "x", hourly_rate: 65)
+@Store2.employees.create(first_name: "Yvee", last_name: "y", hourly_rate: 75)
+
 # Your code goes here ...
